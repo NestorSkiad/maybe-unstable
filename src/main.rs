99f14635135aac8d83eadfb7562e9eacb554be0f
Nestor Skiadas <nestoras_skiadas@protonmail.com>
2025-03-12 20:10:55 +0100

    prep before branch-fork

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,24 @@ use rss::Channel;
 use std::error::Error;
 use std::hash::Hasher;
 use std::io::BufReader;
-use ureq;
 use ahash::AHasher;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
-
-    let mut response =
-        ureq::get("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml").call()?;
-
-    let chn = Channel::read_from(BufReader::new(response.body_mut().as_reader()))?;
+    let sample_data = include_str!("../sample input.xml");
+    let chn = Channel::read_from(BufReader::new(sample_data.as_bytes()))?;
     if chn.items.is_empty() {
         println!("No items found!");
     }
 
     for item in chn.items.into_iter() {
         //println!("{}", item.guid().unwrap().value());
-        println!("{}", item.title().unwrap());
+        println!("{:?}", item.title());
         let mut hasher = AHasher::default();
-        let b = item.guid().unwrap().value().as_bytes();
-        assert!(!b.is_empty());
-        hasher.write(b);
-        println!("Hash: {:?} \n", hasher.finish());
+        //let b = item.guid().unwrap().value().as_bytes();
+        //assert!(!b.is_empty());
+        hasher.write(item.link().unwrap().as_bytes());
+        println!("Hash: {:?}\n", hasher.finish());
         //println!("end of item");
     }
 
